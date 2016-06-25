5fce300d17c0ef5aba7ec7462c546f06b5206a68
Mark Plitt <markplitt@Marks-MacBook-2.local>
2016-06-25 13:00:08 -0400

    first commit

--- tstsubworld.py
+++ tstsubworld.py
@@ -1,6 +1,7 @@
 from neuron import h
 pc = h.ParallelContext()
-pc.subworlds(1)
+pc.subworlds(2) # one subworld 
+            # eg: if we ran this with 3 hosts, rank of subworld = 3
 
 s =  'world' + str((pc.id_world(), pc.nhost_world()))+ ' bbs'+ str((pc.id_bbs(), pc.nhost_bbs())) \
  +' net'+ str((pc.id(),  pc.nhost()))
@@ -14,8 +15,7 @@ for ncell in range(5, 10):
   pc.submit(runring, ncell, 1, 100)
 
 while (pc.working()):
-  print pc.pyret()  
+  print pc.pyret()
 
 pc.done()
 h.quit()
-
